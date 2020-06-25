09800e3791ea36af2d03345c05f91bc8da28f577
nabf1 <nabf1@192.168.2.10>
2020-06-25 15:23:45 -0700

    TRYING TO GET THIS RIGHT!!!!!
    OMG I am in level 1 module 1!!!
    CELEBRATION!!!!!!

--- src/_01_race_car/RaceCarRunner.java
+++ src/_01_race_car/RaceCarRunner.java
@@ -8,10 +8,10 @@ package _01_race_car;
 
 public class RaceCarRunner {
 	public static void main(String[] args) {
-		/* Do the following things without changing the RaceCar class */
-		
+		/* Do the following things without changing the RasceCar class */
+
 		// 1. Create a RaceCar and place it in 5th position
-	
+	//new RaceCar().getClass();
 		// 2. Print the RaceCar's position in the race
 
 		// 3. Crash the RaceCar
@@ -19,6 +19,7 @@ public class RaceCarRunner {
 		// 4. If the car is damaged. Bring it in for a pit stop.
 
 		// 5. Help the car move into first place.
-
+		}
 	}
-}
+
+	
