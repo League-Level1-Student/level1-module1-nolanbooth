f74063c0540d8927b6a329d55b861a0ec95cae90
nabf1 <nabf1@192.168.2.10>
2020-07-24 16:59:24 -0700

    :D

---
+++ src/_07_binary_converter/TheConverter.java
@@ -0,0 +1,20 @@
+package _07_binary_converter;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+public class TheConverter {
+//:D
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button = new JButton();
+	JTextField field  = new JTextField();
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/_07_binary_converter/TheConverterRunner.java
@@ -0,0 +1,5 @@
+package _07_binary_converter;
+
+public class TheConverterRunner {
+
+}
